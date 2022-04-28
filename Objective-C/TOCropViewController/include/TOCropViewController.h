a018ee5b3b76a3073e521810d80d8b384c13e1e2
amirthytejeshwar-sc <92975117+amirthytejeshwar-sc@users.noreply.github.com>
2022-04-28 08:51:14 +0530

    Update TOCropViewController.h

--- Objective-C/TOCropViewController/include/TOCropViewController.h
+++ Objective-C/TOCropViewController/include/TOCropViewController.h
@@ -1,15 +1,15 @@
 #import <UIKit/UIKit.h>
 
-#if SWIFT_PACKAGE
+// #if SWIFT_PACKAGE
 #import "ViewController.h"
 #import "TOCropView.h"
 #import "TOCropToolbar.h"
 #import "TOCropViewConstants.h"
 #import "UIImage+CropRotate.h"
-#else
-#import <TOCropViewController/ViewController.h>
-#import <TOCropViewController/TOCropView.h>
-#import <TOCropViewController/TOCropToolbar.h>
-#import <TOCropViewController/TOCropViewConstants.h>
-#import <TOCropViewController/UIImage+CropRotate.h>
-#endif
+// #else
+// #import <TOCropViewController/ViewController.h>
+// #import <TOCropViewController/TOCropView.h>
+// #import <TOCropViewController/TOCropToolbar.h>
+// #import <TOCropViewController/TOCropViewConstants.h>
+// #import <TOCropViewController/UIImage+CropRotate.h>
+// #endif
