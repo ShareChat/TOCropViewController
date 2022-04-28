91624c08be375d0d766f0a42799144f76d7aebf0
amirthytejeshwar-sc <92975117+amirthytejeshwar-sc@users.noreply.github.com>
2022-04-28 08:45:23 +0530

    Update and rename Objective-C/TOCropViewController/TOCropViewController.h to Objective-C/TOCropViewController/include/TOCropViewController.h

--- Objective-C/TOCropViewController/TOCropViewController.h
+++
@@ -1,7 +0,0 @@
-#import <UIKit/UIKit.h>
-
-#import "ViewController.h"
-#import "TOCropView.h"
-#import "TOCropToolbar.h"
-#import "TOCropViewConstants.h"
-#import "UIImage+CropRotate.h"
---
+++ Objective-C/TOCropViewController/include/TOCropViewController.h
@@ -0,0 +1,16 @@
+#import <UIKit/UIKit.h>
+
+#if SWIFT_PACKAGE
+#import "ViewController.h"
+#import "TOCropView.h"
+#import "TOCropToolbar.h"
+#import "TOCropViewConstants.h"
+#import "UIImage+CropRotate.h"
+#else
+#import <MyModule/SomeHeader.h>
+#import <TOCropViewController/ViewController.h>
+#import <TOCropViewController/TOCropView.h>
+#import <TOCropViewController/TOCropToolbar.h>
+#import <TOCropViewController/TOCropViewConstants.h>
+#import <TOCropViewController/UIImage+CropRotate.h>
+#endif
